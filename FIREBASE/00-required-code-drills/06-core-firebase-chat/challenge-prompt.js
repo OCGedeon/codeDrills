6c53a3cc6745b8d58e16144b9ad9875c9fb6040c
OmarCharlesGedeon <charles@effortlesshost.com>
2018-12-28 16:16:02 -0800

    adding Ajax, Timers & Firebase

---
+++ FIREBASE/00-required-code-drills/06-core-firebase-chat/challenge-prompt.js
@@ -0,0 +1,36 @@
+// Firebase Chat
+
+// creating a variable that references our database, so we don't have to type
+// it out every time
+var database = firebase.database();
+
+// Document.ready shorthand
+$(function() {
+
+  // -------------------- Your Code Here --------------------
+
+   database.ref().on("child_added", function (childSnapshot) {
+
+    var chatItem = $("<p>");
+
+    chatItem.text(childSnapshot.val());
+    $("#chat-display").append(chatItem);
+
+   })
+  
+    $(document).on(
+      "click", "button[type=submit]", function (event) {
+        event.preventDefault();
+
+        var chatMessage = $("input[type=text]").val().trim();
+        $("#chat-input").val("");
+
+        database.ref().push(chatMessage);
+      }
+    )
+ 
+
+  // --------------------- End Code Area --------------------
+
+})
+
---
+++ FIREBASE/01-optional-code-drills/02-rock-hamming-distance/challenge-prompt.js
@@ -0,0 +1,43 @@
+// Hamming Distance
+
+// This functions returns the number of differences between two strings of
+// equal length
+function hamming(str1, str2) {
+
+  // -------------------- Your Code Here --------------------
+
+  if (str1.length !== str2.length) {
+
+    return "Please pass two strings of equal length to this function"
+  }
+  
+  var hamm = 0;
+
+  for (var i = 0 ; i < str1.length ; i++) {
+    
+    if (str1[i] !== str2[i]) {
+     
+      hamm++
+    }
+  }
+
+  return hamm
+  // --------------------- End Code Area --------------------
+
+}
+
+
+// ------------------------------------------------------------------
+console.log("==================== Test 01 ====================");
+console.log("The following should be 5 ");
+console.log(hamming("chicken","charity"));
+
+// ------------------------------------------------------------------
+console.log("==================== Test 02 ====================");
+console.log("The following should be 8");
+console.log(hamming("48ndqw13","6543fedw"));
+
+// ------------------------------------------------------------------
+console.log("==================== Test 03 ====================");
+console.log("The following should be \"Please pass two strings of equal length to this function\"");
+console.log(hamming("dfvs1","dcdw"));
